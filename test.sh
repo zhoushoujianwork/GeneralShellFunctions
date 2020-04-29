d1ac02c51fe352e43f55b4d581caadb34aa44259
zhoushoujian <zhoushoujianwork@163.com>
2020-04-29 17:15:38 +0800

    新增精度条和安卓IMEI号

--- test.sh
+++ test.sh
@@ -7,4 +7,7 @@ LOGOUT=./log/shellcmd.log
 # logout_fileandterminal "这是个日志"
 # echo_err "错误！这是个错误日志"
 # del_dir_file_date /var/log 20
-del_dir_file_date /var/log 10
+# del_dir_file_date /var/log 10
+progress_bar 1 20
+echo 
+android_imei
